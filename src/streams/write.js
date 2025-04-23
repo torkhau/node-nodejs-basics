6d56357321262b8d2e08843b7997d337da8854e5
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-04-23 20:19:50 +0200

    feat: implement read, transform, and write stream functions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,8 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const write = async () => {
-    // Write your code here 
+  pipeline(process.stdin, createWriteStream('./src/streams/files/fileToWrite.txt'));
 };
 
 await write();
