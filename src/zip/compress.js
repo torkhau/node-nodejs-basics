b1ea59f32581f32e2749f7e7f745108e3a934ad2
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-04-23 20:23:32 +0200

    feat: implement compression and decompression functions using streams

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  pipeline(
+    createReadStream('./src/zip/files/fileToCompress.txt'),
+    createGzip(),
+    createWriteStream('./src/zip/files/archive.gz')
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createUnzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  pipeline(
+    createReadStream('./src/zip/files/archive.gz'),
+    createUnzip(),
+    createWriteStream('./src/zip/files/fileToCompress.txt')
+  );
 };
 
 await decompress();
