40b8d68da386af62052107ffb050cda0bcf73a38
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-04-23 21:53:50 +0200

    feat: implement worker threads for parallel Fibonacci calculations

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = Array.from(
+    { length: cpus().length },
+    (_, i) =>
+      new Promise((resolve) => {
+        const worker = new Worker('./src/wt/worker.js', { workerData: 10 + i });
+
+        worker.on('message', (msg) => resolve({ status: 'resolved', data: msg }));
+        worker.on('error', () => resolve({ status: 'error', data: null }));
+        worker.on('exit', (code) => {
+          if (code !== 0) {
+            return resolve({ status: 'error', data: null });
+          }
+        });
+      })
+  );
+
+  const result = await Promise.all(workers);
+
+  console.log(result);
 };
 
 await performCalculations();
